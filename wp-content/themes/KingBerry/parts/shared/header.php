85f62da3d5d129482b468abdaba59ab4615bf0ee
Diego Herrando <diego@theuxdepartment.com>
2014-01-14 21:21:14 -0300

    estructura wp+primeros cambios

---
+++ wp-content/themes/KingBerry/parts/shared/header.php
@@ -0,0 +1,39 @@
+<header>
+	<div class="top-colors"></div>
+	<div class="row header">
+		<div class="large-6 columns">
+			<div class="logo">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.jpg" alt="King Berry SA">
+			</div>
+		</div>
+		<div class="large-6 columns">
+			<div class="search-block">
+				<div class="large-3 columns lang">
+					<a href="#">
+						<span class="flag"></span>English
+					</a>
+				</div>	
+				<div class="large-9 columns">
+					<?php get_search_form(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="nav-content">
+		<div class="row">
+			<nav class="main-menu">
+				<ul>
+					<li>
+						<a href="#">Inicio</a>
+						<a href="#">Nuestra Empresa</a>
+						<a href="#">Nuestros Productos</a>
+						<a href="#">Nuestros Clientes</a>
+						<a href="#">Certificaciones</a>
+						<a href="#">Noticias</a>
+						<a href="#">Contactos</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>		
+</header>
